8b39420862f39bacc8dc00ed1dfc3167f4cdea54
Manishtmg <127338906+Manishtmg@users.noreply.github.com>
2024-10-28 01:15:27 -0400

    Added New Materials and did scripts for changing more colors

---
+++ Assets/Scipts/CharacterCreatorScripts/HairChange.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HairChange : MonoBehaviour
+{
+    [Header("Mesh to Change")]
+    public SkinnedMeshRenderer Hair;
+
+
+
+    [Header("Material to Cycle Through")]
+    public List<Mesh> options = new List<Mesh>();
+
+    private int currentOption = 0;
+    public void nextOption()
+    {
+        currentOption++;
+
+        if (currentOption >= options.Count)
+        {
+
+            currentOption = 0;
+        }
+
+        Hair.sharedMesh = options[currentOption];
+
+    }
+    public void prevOption()
+    {
+        currentOption--;
+        if (currentOption <= 0)
+        {
+
+            currentOption = options.Count - 1;
+        }
+        Hair.sharedMesh = options[currentOption];
+
+
+
+    }
+
+}
